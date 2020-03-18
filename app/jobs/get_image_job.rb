e5ba2ebb2ace998831369c012fa06a9cd414557f
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-19 01:42:10 +0900

    y

--- app/jobs/get_image_job.rb
+++ app/jobs/get_image_job.rb
@@ -12,17 +12,17 @@ class GetImageJob < ApplicationJob
     puts "URL入れた"
     wait.until{ d.find_element(:class, "rakutenLimitedId_ImageMain1-3").displayed? }
     puts "URL入った"
-    # item_name = d.find_element(:class, "item_name").find_element(:tag_name, 'b').text.match(/\(/).pre_match
-    # image = d.find_element(:class, "rakutenLimitedId_ImageMain1-3").find_element(:tag_name, 'img').attribute("src")
-    # max_quantity = d.find_element(:class, 'rItemUnits').find_elements(:tag_name, 'option').length
-    # if expendable.name.blank?
-    #   expendable.name = item_name
-    #   puts "名前入れた"
-    # end
-    # expendable.image = image
-    # expendable.max_quantity = max_quantity
-    # expendable.save
-    # d.get("https://member.id.rakuten.co.jp/rms/nid/logout")
-    # puts "finishsh"
+    item_name = d.find_element(:class, "item_name").find_element(:tag_name, 'b').text.match(/\(/).pre_match
+    image = d.find_element(:class, "rakutenLimitedId_ImageMain1-3").find_element(:tag_name, 'img').attribute("src")
+    max_quantity = d.find_element(:class, 'rItemUnits').find_elements(:tag_name, 'option').length
+    if expendable.name.blank?
+      expendable.name = item_name
+      puts "名前入れた"
+    end
+    expendable.image = image
+    expendable.max_quantity = max_quantity
+    expendable.save
+    d.get("https://member.id.rakuten.co.jp/rms/nid/logout")
+    puts "finishsh"
   end
 end
